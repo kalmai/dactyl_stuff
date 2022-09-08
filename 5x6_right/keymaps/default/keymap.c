02957e40616cce73a3345bf8db41e7ab9ed45f7c
kalmai <leozverres@gmail.com>
2022-09-07 20:09:11 -0400

    rgb working now

--- 5x6_right/keymaps/default/keymap.c
+++ 5x6_right/keymaps/default/keymap.c
@@ -24,8 +24,9 @@ bool set_scrolling = false;
 
 report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
   if (set_scrolling) {
+    // pointing_device_set_cpi();
     mouse_report.h = mouse_report.x;
-    mouse_report.v = mouse_report.y;
+    mouse_report.v = -mouse_report.y;
     mouse_report.x = 0;
     mouse_report.y = 0;
   }
@@ -46,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_BACKSPACE,
      KC_LCTL, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
      KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  , KC_COMM,KC_DOT ,KC_SLSH, KC_RSFT,
-     KC_PGDN, KC_PGUP,MO(2) , MO(1) ,KC_LALT, KC_SPC,                                MO(1) , KC_ENT, KC_LBRC,  KC_RBRC,KC_BACKSLASH,
+     MO(2) , KC_PGDN, KC_PGUP,MO(1) ,KC_LALT, KC_SPC,                                MO(1) , KC_ENT, KC_LBRC,  KC_RBRC,KC_BACKSLASH,
                        KC_BTN1,KC_BTN2, KC_LGUI, DRAG_SCROLL,                          KC_ESC, KC_PSCR, KC_GRV
   ),
 
@@ -54,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5, KC_F6 ,                       KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                    KC_TRNS, KC_HOME, KC_END , KC_TRNS, KC_TRNS, KC_DEL,
      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                    KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT,KC_TRNS, KC_TRNS,
-     KC_TRNS, KC_TRNS, KC_TRNS, KC_CAPS, KC_TRNS, KC_TRNS,                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+     KC_TRNS, KC_TRNS, KC_TRNS, KC_CAPS, KC_TRNS, KC_TRNS,                    KC_BTN1, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                             KC_TRNS, AG_TOGG, KC_TRNS
   ),
@@ -62,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [2] = LAYOUT_5x6_right(
      RGB_TOG, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                    KC_TRNS, KC_MUTE, KC_MPLY, KC_TRNS, KC_TRNS, KC_TRNS,
-     KC_TRNS, KC_TRNS, KC_TRNS, RGB_VAD, RGB_VAI, KC_TRNS,                    KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_TRNS, KC_TRNS,
+     KC_TRNS, KC_TRNS, RGB_MOD, RGB_VAD, RGB_VAI, RGB_RMOD,                   KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_TRNS, KC_TRNS,
      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                    KC_TRNS, KC_MRWD, KC_MFFD, KC_TRNS, KC_TRNS, KC_TRNS,
      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                             KC_TRNS, KC_TRNS, KC_TRNS
